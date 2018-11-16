356513736dceb2ed395e4b2fdb62ea4f1bf12f72
PedroHLC <root@pedrohlc.com>
2018-11-16 12:19:04 -0200

    rename awget; update sublime

--- .config/fish/conf.d/00-user.fish
+++ .config/fish/conf.d/00-user.fish
@@ -1,5 +1,5 @@
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias awget='aria2c -s 16 -x 16 -j 16 -k 1M'
+alias aget='aria2c -s 16 -x 16 -j 16 -k 1M'
 alias :q=exit
 set fish_greeting "Give your heart and sould to me, and life will always be: La Vie en Rose..."
 set -gx PATH $PATH ~/.local/bin
