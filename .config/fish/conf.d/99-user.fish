5cec76778c1b18c51d34224141c8446727126d1a
PedroHLC <root@pedrohlc.com>
2020-07-18 10:26:03 -0300

    Accumulative 2020-07-18

--- .config/fish/conf.d/99-user.fish
+++ .config/fish/conf.d/99-user.fish
@@ -1,9 +1,8 @@
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-alias aget='aria2c -s 16 -x 16 -j 16 -k 1M'
 alias :q=exit
+alias aget='aria2c -s 16 -x 16 -j 16 -k 1M'
 alias set-vulkan-intel='export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json'
-alias set-gaming='export DXVK_HUD=compiler MANGOHUD=1 PROTON_WINEDBG_DISABLE=1'
 
 set fish_greeting "Give your heart and soul to me, and life will always be: La Vie en Rose..."
 set -gx PATH $PATH ~/.local/bin
