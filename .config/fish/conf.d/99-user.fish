4ae8bae72bf3489b054c5a3f3d2ff1347d15974a
PedroHLC <root@pedrohlc.com>
2020-08-27 18:32:21 -0300

    Hanewaka greeting; Remove memory constraints from apps; just 8bit on screensaver; use more variables on sway

--- .config/fish/conf.d/99-user.fish
+++ .config/fish/conf.d/99-user.fish
@@ -4,5 +4,5 @@ alias :q=exit
 alias aget='aria2c -s 16 -x 16 -j 16 -k 1M'
 alias set-vulkan-intel='export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json'
 
-set fish_greeting "Give your heart and soul to me, and life will always be: La Vie en Rose..."
+set fish_greeting '何でもは知らないわよ。知ってることだけ'
 set -gx PATH $PATH ~/.local/bin
