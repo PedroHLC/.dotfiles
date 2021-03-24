8c8fe9eae2296f3633a96928cf686e49088d42a0
PedroHLC <root@pedrohlc.com>
2021-03-24 20:09:27 -0300

    Accumulated changes

--- .config/fish/conf.d/99-user.fish
+++ .config/fish/conf.d/99-user.fish
@@ -2,7 +2,7 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 alias :q=exit
 alias aget='aria2c -s 16 -x 16 -j 16 -k 1M'
-alias set-vulkan-intel='export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json'
+alias hqmpv='mpv --profile=hqvideo'
 
 set fish_greeting '何でもは知らないわよ。知ってることだけ'
 set -gx PATH $PATH ~/.local/bin
