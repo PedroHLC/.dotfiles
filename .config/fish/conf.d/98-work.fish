e14dbe3eb777a9a107e57ba7157c8e9da8fe2624
PedroHLC <root@pedrohlc.com>
2021-10-19 13:20:52 -0300

    Sync 2021-10-19

--- .config/fish/conf.d/98-work.fish
+++ .config/fish/conf.d/98-work.fish
@@ -3,4 +3,5 @@ alias elm='yarn exec --offline -- elm'
 alias elm-review='yarn exec --offline -- elm-review'
 alias elm-app='yarn exec --offline -- elm-app'
 alias elm-test='yarn exec --offline -- elm-test'
+alias elm-optimize-level-2='yarn exec --offline -- elm-optimize-level-2'
 
