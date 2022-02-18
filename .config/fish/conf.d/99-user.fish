6f37db0bb8b9e864e741ab5da5c233b271d24170
PedroHLC <root@pedrohlc.com>
2022-02-17 22:47:52 -0300

    battery in-game; update alacritty; use "umpv"

--- .config/fish/conf.d/99-user.fish
+++ .config/fish/conf.d/99-user.fish
@@ -3,7 +3,7 @@ alias my-mkrootfs='git --git-dir=$HOME/Projects/com.pedrohlc/my-mkrootfs --work-
 
 alias :q=exit
 alias aget='aria2c -s 16 -x 16 -j 16 -k 1M'
-alias hqmpv='mpv --profile=hqvideo'
+alias hqmpv='umpv --profile=hqvideo'
 
 set fish_greeting '何でもは知らないわよ。知ってることだけ'
 set -gx PATH $PATH ~/.local/bin
