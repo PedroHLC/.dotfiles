b44cc805f2fe7a3e99d423db49aea4d905f9b49b
PedroHLC <root@pedrohlc.com>
2022-03-18 18:12:19 -0300

    Borg sans mono; more stuff to work env; package cycling

--- .config/fish/conf.d/98-work.fish
+++ .config/fish/conf.d/98-work.fish
@@ -4,4 +4,5 @@ alias elm-review='yarn exec --offline -- elm-review'
 alias elm-app='yarn exec --offline -- elm-app'
 alias elm-test='yarn exec --offline -- elm-test'
 alias elm-optimize-level-2='yarn exec --offline -- elm-optimize-level-2'
+alias elm-graphql='yarn exec --offline -- elm-graphql'
 
